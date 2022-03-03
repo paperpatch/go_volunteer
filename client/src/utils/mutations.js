0b190c7d018c61655f5e11dc91ed8eb1437abb22
paperpatch <patchen21@gmail.com>
2022-03-03 11:46:20 -0500

    updated client query and mutation template

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -11,24 +11,6 @@ export const LOGIN = gql`
   }
 `;
 
-export const ADD_ORDER = gql`
-  mutation addOrder($products: [ID]!) {
-    addOrder(products: $products) {
-      purchaseDate
-      products {
-        _id
-        name
-        description
-        price
-        quantity
-        category {
-          name
-        }
-      }
-    }
-  }
-`;
-
 export const ADD_USER = gql`
   mutation addUser(
     $firstName: String!
@@ -45,6 +27,198 @@ export const ADD_USER = gql`
       token
       user {
         _id
+      }
+    }
+  }
+`;
+
+export const CREATE_EVENT = gql`
+  mutation createEvent(
+    $title: String!
+    $location: String!
+    $description: String!
+    $date: String!
+    $startTime: String!
+    $endTime: String!
+    $url: String!
+    $image: String!
+  ) {
+    createEvent(
+      title: $title
+      location: $location
+      description: $description
+      date: $date
+      startTime: $startTime
+      endTime: $endTime
+      url: $url
+      image: $image
+    ) {
+      _id
+      host {
+        _id
+      }
+      title
+      location
+      description
+      date
+      startTime
+      endTime
+      url
+      image
+    }
+  }
+`;
+
+export const JOIN_EVENT = gql`
+  mutation joinEvent($eventId: ID!) {
+    joinEvent(eventId: $eventId) {
+      _id
+      host {
+        _id
+      }
+      title
+      attendees {
+        _id
+      }
+      location
+      description
+      date
+      startTime
+      endTime
+      url
+      image
+    }
+  }
+`;
+
+export const LEAVE_EVENT = gql`
+  mutation leaveEvent($eventId: ID!) {
+    leaveEvent(eventId: $eventId) {
+      _id
+      host {
+        _id
+      }
+      title
+      attendees {
+        _id
+      }
+      location
+      description
+      date
+      startTime
+      endTime
+      url
+      image
+      comments {
+        _id
+        author {
+          _id
+        }
+        commentText
+        likes
+        replies {
+          _id
+          author {
+            _id
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const CANCEL_EVENT = gql`
+  mutation Mutation($eventId: ID!) {
+    cancelEvent(eventId: $eventId) {
+      _id
+      title
+    }
+  }
+`;
+
+export const ADD_EVENT_COMMENT = gql`
+  mutation Mutation($commentText: String!, $eventId: ID) {
+    addEventComment(commentText: $commentText, eventId: $eventId) {
+      _id
+      host {
+        _id
+        firstName
+        lastName
+      }
+      title
+      attendees {
+        _id
+      }
+      location
+      description
+      date
+      startTime
+      endTime
+      url
+      comments {
+        _id
+        author {
+          _id
+        }
+        commentText
+        createdAt
+        likes
+      }
+      image
+    }
+  }
+`;
+
+export const ADD_REPLY = gql`
+  mutation AddReply($commentId: ID!, $replyBody: String!) {
+    addReply(commentId: $commentId, replyBody: $replyBody) {
+      _id
+      commentText
+      replies {
+        replyBody
+        author {
+          firstName
+        }
+      }
+    }
+  }
+`;
+
+export const REMOVE_COMMENT = gql`
+  mutation removeComment($commentId: ID!, $eventId: ID, $goodDeedId: ID) {
+    removeComment(
+      commentId: $commentId
+      eventId: $eventId
+      goodDeedId: $goodDeedId
+    ) {
+      _id
+      host {
+        _id
+      }
+      title
+      attendees {
+        _id
+      }
+      location
+      description
+      date
+      startTime
+      endTime
+      url
+      image
+      comments {
+        _id
+        author {
+          _id
+        }
+        commentText
+        replies {
+          _id
+          author {
+            _id
+          }
+          replyBody
+        }
       }
     }
   }
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,40 +1,246 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_PRODUCTS = gql`
-  query getProducts($category: ID) {
-    products(category: $category) {
-      _id
-      name
-      description
-      price
-      quantity
-      image
-      category {
-        _id
-      }
-    }
-  }
-`;
-
-export const QUERY_CHECKOUT = gql`
-  query getCheckout($products: [ID]!) {
-    checkout(products: $products) {
-      session
-    }
-  }
-`;
-
-export const QUERY_ALL_PRODUCTS = gql`
+export const QUERY_ME = gql`
   {
-    products {
+    me {
       _id
-      name
-      description
-      price
-      quantity
-      category {
-        name
+      firstName
+      lastName
+      email
+      location
+      profilePicture
+      events {
+        _id
+        host {
+          _id
+          firstName
+          lastName
+        }
+        title
+        location
+        description
+        date
+        startTime
+        endTime
+        url
+        image
+        attendees {
+          _id
+        }
       }
+    }
+  }
+`;
+
+export const QUERY_USER = gql`
+  query Query($id: ID!) {
+    user(_id: $id) {
+      _id
+      firstName
+      lastName
+      location
+      profilePicture
+      connections {
+        _id
+        firstName
+        lastName
+      }
+      events {
+        _id
+        host {
+          _id
+          lastName
+          firstName
+        }
+        title
+        attendees {
+          _id
+        }
+        location
+        description
+        startTime
+        date
+        endTime
+        url
+      }
+    }
+  }
+`;
+
+export const QUERY_USERS = gql`
+  query users {
+    users {
+      _id
+      firstName
+      lastName
+      email
+      location
+      profilePicture
+      connections {
+        _id
+        firstName
+        lastName
+      }
+      events {
+        _id
+        host {
+          _id
+          firstName
+          lastName
+        }
+        title
+        location
+        description
+        date
+        startTime
+        endTime
+        url
+        image
+      }
+    }
+  }
+`;
+
+export const QUERY_EVENT = gql`
+  query Event($id: ID) {
+    event(_id: $id) {
+      _id
+      host {
+        _id
+        firstName
+        lastName
+      }
+      title
+      attendees {
+        _id
+        firstName
+        lastName
+      }
+      location
+      isVerified
+      description
+      date
+      startTime
+      endTime
+      url
+      image
+      comments {
+        _id
+        author {
+          lastName
+          firstName
+          _id
+        }
+        replies {
+          _id
+          author {
+            firstName
+            _id
+            lastName
+          }
+          replyBody
+          createdAt
+        }
+        commentText
+        createdAt
+      }
+      eventLikes {
+        _id
+        eventLikes
+        user {
+          _id
+        }
+        event {
+          _id
+        }
+      }
+      verify {
+        _id
+        user {
+          _id
+        }
+        event {
+          _id
+        }
+        verifyNumber
+      }
+    }
+  }
+`;
+
+export const QUERY_EVENTS = gql`
+  {
+    events {
+      _id
+      host {
+        _id
+        firstName
+        lastName
+      }
+      title
+      attendees {
+        _id
+        firstName
+        lastName
+      }
+      location
+      description
+      date
+      isVerified
+      startTime
+      verify {
+        _id
+        user {
+          _id
+        }
+        event {
+          _id
+        }
+        verifyNumber
+      }
+      endTime
+      url
+      image
+      comments {
+        _id
+        commentText
+        createdAt
+        replies {
+          _id
+          replyBody
+        }
+        author {
+          _id
+          firstName
+          lastName
+        }
+      }
+      eventLikes {
+        _id
+        user {
+          _id
+        }
+        eventLikes
+        event {
+          _id
+        }
+      }
+    }
+  }
+`;
+
+export const QUERY_COMMENTS = gql`
+  {
+    comments {
+      _id
+      author {
+        _id
+        firstName
+        lastName
+      }
+      commentText
+      createdAt
+      likes
     }
   }
 `;
@@ -44,27 +250,6 @@ export const QUERY_CATEGORIES = gql`
     categories {
       _id
       name
-    }
-  }
-`;
-
-export const QUERY_USER = gql`
-  {
-    user {
-      firstName
-      lastName
-      orders {
-        _id
-        purchaseDate
-        products {
-          _id
-          name
-          description
-          price
-          quantity
-          image
-        }
-      }
     }
   }
 `;
